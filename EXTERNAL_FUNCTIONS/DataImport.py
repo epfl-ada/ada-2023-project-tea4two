b9aaccb9b34ac03473cf478c3c1db1b15405c9fd
pauluspolo <118297696+pauluspolo@users.noreply.github.com>
2023-11-16 00:42:00 +0100

    exploration data

--- EXTERNAL_FUNCTIONS/DataImport.py
+++ EXTERNAL_FUNCTIONS/DataImport.py
@@ -2,11 +2,11 @@ import os
 import json
 import pandas as pd
 
-def OpenDF(PATH) :
+def OpenMainMovieDatasetDF(PATH):
     '''
     This function opens the dataframes stored in the file with location PATH
     '''
-
+    
     character_file = os.path.join(PATH, 'character.metadata.tsv')
     movie_file = os.path.join(PATH, 'movie.metadata.tsv')
     name_file = os.path.join(PATH, 'name.clusters.txt')
@@ -20,7 +20,7 @@ def OpenDF(PATH) :
     character_col = ["Wikipedia movie ID", "Freebase movie ID", "Movie release date", "Character Name", "Actor DOB",
                      "Actor gender", "Actor height", "Actor ethnicity", "Actor Name",
                      "Actor age at movie release", "Freebase character map"]
-    name_col = ["Character Name", "Freebase character map ?"]
+    name_col = ["Character Name", "Freebase character map"]
 
     plot_col = ["Wikipedia movie ID", "Plot"]
 
@@ -40,3 +40,23 @@ def OpenDF(PATH) :
         tvtropDF = pd.concat([tvtropDF, pd.DataFrame([dico])], ignore_index=True)
 
     return characterDF, movieDF, nameDF, plotDF, tvtropDF
+
+
+def FillMissingValues(target_df, source_df, target_column, source_column):
+    # Calculate percentage of missing values before filling
+    before_fill_percentage = target_df[target_column].isnull().mean() * 100
+    
+    target_df[target_column] = target_df[target_column].fillna(source_df[source_column])
+
+    # Calculate percentage of missing values after filling
+    after_fill_percentage = target_df[target_column].isnull().mean() * 100
+
+    # Calculate the percentage improvement
+    improvement_percentage = before_fill_percentage - after_fill_percentage
+
+    # Print the results
+    print(f"Percentage of missing values before fill: {before_fill_percentage:.2f}%")
+    print(f"Percentage of missing values after fill: {after_fill_percentage:.2f}%")
+    print(f"Percentage improvement: {improvement_percentage:.2f}%")
+
+    return target_df
---
+++ EXTERNAL_FUNCTIONS/DatasetTreatment.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import ast
+
+def split_dict(row):
+    '''
+    This function splits a dictonnary in two distinct lists : list of the keys , list of the values 
+    '''
+    if not row : 
+        keys, values = [], []
+    else : 
+        keys, values = zip(*row.items())
+    return pd.Series({'Keys': list(keys), 'Values': list(values)})
+
+def MovieDF_Treatment(MovieDF) :
+    '''
+    This function deals with the dataframe MovieDF
+    '''
+    df = MovieDF.copy(deep = True)
+    #Movie languages 
+    df['Movie languages'] = df['Movie languages'].apply(ast.literal_eval)
+    df[['Freebase Language', 'Movie languages']] = df['Movie languages'].apply(split_dict)
+    
+    #Movie countrty 
+    df['Movie countries'] = df['Movie countries'].apply(ast.literal_eval)
+    df[['Freebase country', 'Movie countries']] = df['Movie countries'].apply(split_dict)
+    #Movie genres 
+    
+    df['Movie genres'] = df['Movie genres'].apply(ast.literal_eval)
+    df[['Freebase genre', 'Movie genres']] = df['Movie genres'].apply(split_dict)
+    return df
