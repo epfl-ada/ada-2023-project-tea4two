849263143a3e5151ed3305444aa706043cbe087d
pauluspolo <118297696+pauluspolo@users.noreply.github.com>
2023-12-22 11:36:27 +0100

    color
    Co-authored-by: JeanLefort <JeanLefort@users.noreply.github.com>
    Co-authored-by: GuillaumeSalha <GuillaumeSalha@users.noreply.github.com>
    Co-authored-by: HugoG00 <HugoG00@users.noreply.github.com>

--- EXTERNAL_FUNCTIONS/SaveOrShowFigure.py
+++ EXTERNAL_FUNCTIONS/SaveOrShowFigure.py
@@ -1,7 +1,13 @@
 OUTPUT_FOLDER_HTML = './IMAGES/html/'
 OUTPUT_FOLDER_WEBSITE_HTML = '../jeanlefort.github.io/img/html/'
+OUTPUT_FOLDER_PNG = './IMAGES/png/'
+OUTPUT_FOLDER_WEBSITE_PNG = '../jeanlefort.github.io/img/png/'
+
 def export_plots(fig, name):
     fig.write_html(OUTPUT_FOLDER_HTML + name + ".html")
     fig.write_html(OUTPUT_FOLDER_WEBSITE_HTML + name + ".html")
+    #fig.write_image(OUTPUT_FOLDER_PNG + name + ".png")
+    #fig.write_image(OUTPUT_FOLDER_WEBSITE_PNG + name + ".png")
 
+    # return Image(output_img_folder + name + ".png")
     return fig.show()
