889243808776f3c6f44b28e20e52f269c1b855f3
JeanLefort <jean.lefort@polytechnique.edu>
2023-12-22 17:10:11 +0100

    export gender html

--- EXTERNAL_FUNCTIONS/SaveOrShowFigure.py
+++ EXTERNAL_FUNCTIONS/SaveOrShowFigure.py
@@ -6,8 +6,8 @@ OUTPUT_FOLDER_WEBSITE_PNG = '../jeanlefort.github.io/img/png/'
 def export_plots(fig, name):
     fig.write_html(OUTPUT_FOLDER_HTML + name + ".html")
     fig.write_html(OUTPUT_FOLDER_WEBSITE_HTML + name + ".html")
-    #fig.write_image(OUTPUT_FOLDER_PNG + name + ".png")
-    #fig.write_image(OUTPUT_FOLDER_WEBSITE_PNG + name + ".png")
+    # fig.write_image(OUTPUT_FOLDER_PNG + name + ".png")
+    # fig.write_image(OUTPUT_FOLDER_WEBSITE_PNG + name + ".png")
 
     # return Image(output_img_folder + name + ".png")
     return fig.show()
